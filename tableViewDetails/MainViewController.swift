3768f16798225f06430404f0b6c1d16f111552cd
szilardinho <szilardinhog@gmail.com>
2017-04-01 02:06:49 -0400

    tableViewCell customized

---
+++ tableViewDetails/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewDetails
+//
+//  Created by Szi Gabor on 4/1/17.
+//  Copyright © 2017 Szi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
+
